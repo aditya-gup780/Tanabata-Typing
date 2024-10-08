131ec25b364f3dab31b54b55773472e144b24e16
cicada3298 <123466277+cicada3298@users.noreply.github.com>
2024-10-08 15:03:22 +0530

    Update app.js

--- BACKENED/app.js
+++ BACKENED/app.js
@@ -11,6 +11,26 @@ const QuotableAPI = require('./QuotableAPI');
 
 io.connect('connect',(socket)=>{
 
+  socket.on('timer', async({gameID,playerID})=>{
+    let countDown = 5;
+    let game = await Game.findById(gameID);
+    let player = game.players.id(playerID);
+    if(player.isPartyLeader){
+      let timerID = setInterval(async()=>{
+        if(countDown >= 0){
+          io.to(gameID).emit('timer',{countDown,msg : "Starting game"});
+          countDown--;
+        }
+        else{
+          game.isOpen = false;
+          game = await game.save();
+          io.to(gameID).emit('updateGame',game);
+          startGameClock(gameID);
+          clearInterval(timerID);
+        }
+      },1000);
+    }
+  })
   socket.on('join-game',async({gameID : _id, nickName})=>{
     try{
       let game = await Game.findById(_id);
@@ -50,4 +70,47 @@ io.connect('connect',(socket)=>{
       console.log(err);
     }
   })
-})
+});
+
+const startGameClock = async (gameID) => {
+  let game = await Game.findById(gameID);
+  game.startTime = new Date().getTime();
+  game = await game.save();
+  let time = 120;
+  let timerID = setInterval(function gameIntervalFunc(){
+    if(time >= 0){
+      const formatTime = calculateTime(time);
+      io.to(gameID).emit('timer',{countDown : formatTime, msg : "Time Remaining"});
+      time--;
+    }
+    else{
+      (async ()=>{
+        let endTime = new Date().getTime();
+        let game = await Game.findById(gameID);
+        let {startTime} = game;
+        game.isOver = true;
+        game.players.forEach((player,index)=>{
+          if(player.WPM === -1)
+            game.players[index].WPM = calculateWPM(endTime,startTime,player);
+        });
+        game  = await game.save();
+        io.to(gameID).emit('updateGame',game);
+        clearInterval(timerID);
+      })()
+    }
+    return gameIntervalFunc;
+  }(),1000);
+}
+const calculateTime = (time) => {
+  let minutes = Math.floor(time/60);
+  let seconds = time%60;
+  return `${minutes}:${seconds < 10 ? "0" + seconds : seconds}`;
+}
+
+const calculateWPM = (endTime,startTime,player) => {
+  let numOfWords = player.currentWordIndex;
+  const timeInSeconds = (endTime-startTime)/1000;
+  const timeInMinutes = timeInSeconds/60;
+  const WPM = Math.floor(numOfWords/timeInMinutes);
+  return WPM;
+}
