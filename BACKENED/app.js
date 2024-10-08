3db9405ab4ff9a2d4496ceddd599917d2c6cea63
cicada3298 <123466277+cicada3298@users.noreply.github.com>
2024-10-08 11:23:40 +0530

    Update app.js

--- BACKENED/app.js
+++ BACKENED/app.js
@@ -10,6 +10,26 @@ const Game = require('./Game');
 const QuotableAPI = require('./QuotableAPI');
 
 io.connect('connect',(socket)=>{
+
+  socket.on('join-game',async({gameID : _id, nickName})=>{
+    try{
+      let game = await Game.findById(_id);
+      if(game.isOpen){
+        const gameID = game._id.toString();
+        socket.join(gameID);
+        let player = {
+          socketID : socket.id,
+          nickName
+        }
+        game.players.push(player);
+        game = await game.save();
+        io.to(gameID).emit('updateGame',game);
+      }  
+    }catch(err){
+      console.log(err);
+    }
+  });
+  
   socket.on('create-game',async(nickName)=>{
     try{
       const quotableData = await QuotableAPI();
