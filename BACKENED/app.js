61bdd3fb7a1ca6446e163ee0ce14eab8737c5d40
cicada3298 <123466277+cicada3298@users.noreply.github.com>
2024-10-09 00:05:07 +0530

    Update app.js

--- BACKENED/app.js
+++ BACKENED/app.js
@@ -11,6 +11,31 @@ const QuotableAPI = require('./QuotableAPI');
 
 io.connect('connect',(socket)=>{
 
+  socket.on('userInput', async({userInput,gameID})=>{
+    try{
+      let game = await Game.findById(gameID);
+      if(!game.isOpen && !game.isOver){
+        let player = game.players.find(player => player.socketID === socket.id);
+        let word = game.words[player.currentWordIndex];
+        if(word === userInput){
+          player.currentWordIndex++;
+          if(player.currentWordIndex !== game.words.length){
+            game = await game.save();
+            io.to(gameID).emit('updateGame',game);
+          }
+          else{
+            let endTime = new Date().getTime();
+            let {startTime} = game;
+            player.WPM = calculateWPM(endTime,startTime,player);
+            game = await game.save();
+            socket.emit('done');
+            io.to(gameID).emit('updateGame',game);
+          }
+        }
+      }
+    }
+  })
+
   socket.on('timer', async({gameID,playerID})=>{
     let countDown = 5;
     let game = await Game.findById(gameID);
