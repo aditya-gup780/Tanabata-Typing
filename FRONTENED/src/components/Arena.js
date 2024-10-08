8498abd1a8facb4032b8c6a607902f747d60e6f8
cicada3298 <123466277+cicada3298@users.noreply.github.com>
2024-10-09 01:33:25 +0530

    Update Arena.js

--- FRONTENED/src/components/Arena.js
+++ FRONTENED/src/components/Arena.js
@@ -5,6 +5,9 @@ import StartBtn from './StartBtn';
 import socket from '../socketConfig';
 import DisplayWords from './DisplayWords';
 import Form from './Form';
+import progressBar from './ProgressBar';
+import ScoreBoard from './ScoreBoard';
+import DisplayGameCode from './DisplayGameCode';
 
 const findPlayer = players => {
   return players.find(player => player.socketID === socket.id);
@@ -18,9 +21,12 @@ const Arena = ({gameState}) => {
   return (
     <div className="text-center">
       <DisplayWords words={words} player={player}>
+      <ProgressBar players={palyers} player={player} wordsLength={words.length}>
       <Form isOpen={isOpen} isOver={isOver} gameID={_id}>
       <CountDown />
       <StartBtn player={player} gameID={_id}/>
+      <DisplayGameCode gameID={gameID}>
+      <ScoreBoard players={players}>
     </div>
   )
 }
