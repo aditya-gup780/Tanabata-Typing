f54806d918887db3a44cd37f7055d13e0cf75acf
cicada3298 <123466277+cicada3298@users.noreply.github.com>
2024-10-08 09:15:17 +0530

    Updated App.js

--- FRONTENED/src/App.js
+++ FRONTENED/src/App.js
@@ -1,14 +1,34 @@
 import "./App.css";
-
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, {useEffect, useState} from 'react';
+import { useNavigate, BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import TextArea from "./pages/Textarea/Textarea";
 import Home from "./pages/Home/Home";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Loginsignup from "./pages/loginsignup/loginsignup";
 import LeaderBoard from "./components/Leaderboard";
+import CreateGame from "./components/CreateGame";
+import socket from './socketConfig';
 // import loop from './components/ScrollEffect'
 
 function App() {
+  const navigate = useNavigate();
+  const [gameState,setGameState] = useState({_id : "", isOpen : false, players : [], words : []});
+  
+  useEffect(()=>{
+    socket.on('updateGame',(game)=>{
+      console.log(game);
+      setGameState(game);
+    });
+    return () => {
+      socket.removeAllListeners();
+    }
+  },[]);
+  
+  useEffect(()=>{
+    if(gameState._id !== "")
+      navigate(`/game/${gameState._id}`);
+  }, [gameState._id]); 
+  
   return (
     <div className="App">
       <Router>
@@ -20,6 +40,9 @@ function App() {
         </Routes>
         <Routes>
           <Route exact path="/dashboard" element={<Dashboard></Dashboard>} />
+        </Routes>
+        <Routes>
+          <Route exact path="/game/create" element={<CreateGame></CreateGame>} />
         </Routes>
         <Routes>
           <Route
