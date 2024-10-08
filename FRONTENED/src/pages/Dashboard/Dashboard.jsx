c78a3a5192caa12792ed0415ba88504371516c91
cicada3298 <123466277+cicada3298@users.noreply.github.com>
2024-10-08 07:44:23 +0530

    Update Dashboard.jsx

--- FRONTENED/src/pages/Dashboard/Dashboard.jsx
+++ FRONTENED/src/pages/Dashboard/Dashboard.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import Navbar2 from "../../components/Navbar2/Navbar2";
 import SideList from "../../components/sidelist/SideList";
+import {useNavigate} from 'react-router-dom'; 
 // import Top from '../components/Top';
 // import Mid from '../components/Mid'
 // import Bottom from '../components/Bottom'
 
 const Dashboard = () => {
+  let navigate = useNavigate();
   return (
     <div className="dashboard">
       <Navbar2 />
       <div className="Text">
         <SideList />
+      </div>
+      <div className="text-center">
+        <h1>Welcome to Tanabata-typing</h1>
+        <button type="button" onClick={() => navigate('/game/create')} className="btn btn-primary btn-lg mr-3"> Create Game </button>
+        <button type="button" onClick={() => navigate('/game/create')} className="btn btn-primary btn-lg"> Join Game </button>
       </div>
     </div>
   );
